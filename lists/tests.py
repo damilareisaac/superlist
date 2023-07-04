563ed55b229fbe98cc85311b1598ba8d766c2765
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air.local>
2023-07-04 08:08:16 +0100

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.urls import resolve
+from lists.views import home_page
 
 
 class SmokeTestCase(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+        self.assertEqual(1 + 1, 2)
+
+
+class HomePageTestCase(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from lists import urls as list_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include(list_urls)),
 ]
