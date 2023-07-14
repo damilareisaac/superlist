e147f391429ef00dc1ee14fb19803942f5a32057
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air.local>
2023-07-14 16:29:54 +0100

    New form for list items

---
+++ lists/tests/tests_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from lists.models import Item, List
+
+
+class ListAndItemModelsTestCase(TestCase):
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = "First dummy item"
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Second dummy item"
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+
+        first_item = saved_items[0]
+        second_item = saved_items[1]
+
+        self.assertEqual(first_item.text, "First dummy item")
+        self.assertEqual(first_item.list, list_)
+        self.assertEqual(second_item.text, "Second dummy item")
+        self.assertEqual(second_item.list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text="")
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f"/lists/{list_.id}/")
